a16a69927da3c44b01127ba3297c2ec9bec020ea
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-06-14 15:10:24 +0300

    .

--- AngularSandbox/ClientApp/src/app/app.module.ts
+++ AngularSandbox/ClientApp/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BaseInputOutputComponent } from './base-input-output/base-input-output.component';
--- AngularSandbox/Startup.cs
+++ AngularSandbox/Startup.cs
@@ -1,15 +1,6 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AngularSandbox
 {
@@ -26,7 +17,7 @@ namespace AngularSandbox
         {
             services.AddControllers();
         }
-        
+
         public void Configure(IApplicationBuilder app)
         {
             app.UseDefaultFiles();
